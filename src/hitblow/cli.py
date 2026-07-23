2a280867b0015132010f54ba2418f4ce188e61d7
b240265 <b240265@hiroshima-u.ac.jp>
2026-07-23 11:35:21 +0900

    Update cli.py

--- src/hitblow/cli.py
+++ src/hitblow/cli.py
@@ -1,11 +1,11 @@
 """コマンドの入口。第3回で `hitblow` コマンドがここ（main）を呼ぶ。"""
 
-# from .game import play
+from .game import play
 
-
-# def main():
-#     play()
-from .gui import main as gui_main
 
 def main():
-    gui_main()
+    play()
+# from .gui import main as gui_main
+
+# def main():
+#     gui_main()
